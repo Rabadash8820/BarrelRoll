0239308f7305a47ee219ab1384799fccbc2cb914
Daniel Vicarel <shundra8820@gmail.com>
2016-03-10 16:36:38 -0500

    Created a basic scene with input-rotatable  walls and some debris falling around

---
+++ Assets/Dan's Assets/Scripts/RollingInput.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Rolling {
+
+    class RollingInput {
+        public static float RotateCW { get { return -Input.GetAxis("Horizontal"); } }
+        public static float RotateCWRaw { get { return -Input.GetAxisRaw("Horizontal"); } }
+    }
+
+}
---
+++ Assets/Dan's Assets/Scripts/Rotator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Rolling {
+
+    public class Rotator : MonoBehaviour {
+        // INSPECTOR FIELDS
+        public Transform Target;
+        public float RotationSpeed = 1f;
+
+        // EVENT HANDLERS
+        private void Awake() {
+
+        }
+        private void Update() {
+            // Get player input
+            float rot = RollingInput.RotateCW;
+
+            // Do the rotation
+            float rotMag = rot * RotationSpeed;
+            Vector3 rotAxis = Vector3.forward;
+            doRotate(rotMag, rotAxis);
+        }
+
+        // HELPER FUNCTIONS
+        private void doRotate(float magnitude, Vector3 axis) {
+            // If the Target has a non-kinematic Rigidbody, then apply a Torque
+            Rigidbody2D rb = Target.GetComponent<Rigidbody2D>();
+            if (rb != null && !rb.isKinematic)
+                rb.AddTorque(magnitude);
+
+            // Otherwise, rotate its Transform directly
+            else
+                Target.Rotate(magnitude * axis);
+
+        }
+    }
+
+}
